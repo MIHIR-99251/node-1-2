9c311779dc51fa3dc82299690628018ed7879b17
Mihir Pariya <mpariya559@rku.ac.in>
2021-12-31 09:29:11 +0530

    js node 1 completed

--- js node 1.js
+++ js node 1.js
@@ -1,1 +1,9 @@
 var event = require ('event')
+var em = new EventSource.eventemittem();
+em.on('my event',function(data){
+    count=0;
+    data.foeach(i=>{
+    count=count+i;
+  });
+  console.log(count)
+});
